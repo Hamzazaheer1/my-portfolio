92a784c1ccb22055216c138519322a4a2fc85106
Hamza Zaheer <malikhamza1619619@gmail.com>
2022-10-26 23:35:55 +0500

    testing

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div className="bg-gray-700 w-screen">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
